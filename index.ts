df40d2f6735e7dcbb885e9a7de3e78458bce409d
Rival Msr <rivalmusripal@gmail.com>
2026-01-17 23:48:08 +0700

    chore(setup): Set up Prettier

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import concurrently from 'concurrently'
+import concurrently from 'concurrently';
 
 concurrently([
   {
@@ -6,10 +6,11 @@ concurrently([
     command: 'bun run dev',
     cwd: 'packages/server',
     prefixColor: 'cyan',
-  }, {
+  },
+  {
     name: 'client',
     command: 'bun run dev',
     cwd: 'packages/client',
     prefixColor: 'magenta',
-  }
-])
+  },
+]);
--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,24 +1,26 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import { Button } from './components/ui/button'
+import { useEffect, useState } from 'react';
+import './App.css';
+import { Button } from './components/ui/button';
 
 function App() {
-  const [message, setMessage] = useState('')
+  const [message, setMessage] = useState('');
 
   useEffect(() => {
-    fetch('/api/hello').then(async (req) => {
-      return await req.json()
-    }).then((res) => {
-      setMessage(res.message)
-    })
-  })
+    fetch('/api/hello')
+      .then(async (req) => {
+        return await req.json();
+      })
+      .then((res) => {
+        setMessage(res.message);
+      });
+  });
 
   return (
-    <div className='p-4'>
-      <p className='font-semibold text-2xl'>{message}</p>
+    <div className="p-4">
+      <p className="font-semibold text-2xl">{message}</p>
       <Button>Button</Button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
