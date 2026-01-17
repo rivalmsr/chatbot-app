1d61669981130d4918c433a8e8788025bdcee4e0
Rival Msr <rivalmusripal@gmail.com>
2026-01-17 22:59:46 +0700

    chore(setup): Run both apps together

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -10,6 +10,10 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello World')
 })
 
+app.get('/api/hello', (req: Request, res: Response) => {
+  res.json({ message: 'endpoint hello' })
+})
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`)
 })
